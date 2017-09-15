bc65f9ee49550c3e689327fe0fe66879a0d3c5a2
1hanhb <1hanhb@naver.com>
2017-09-15 11:06:55 +0900

    BOJ java project added

---
+++ BOJ/2156 포도주 시식/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int []wine = new int[n];
+		int [][]max = new int[n][2]; // 0 가능, 1 불가
+		for(int i=0; i<n; i++)
+			wine[i] = scanner.nextInt();
+		if(n==1)
+			System.out.println(wine[0]);
+		else {
+			max[0][0] = wine[0];
+			max[1][0] = wine[1];
+			
+			for(int i=1; i<n; i++) {
+				//2579 계단 오르기와 다른 점 : 세 칸 이상 건너뛸 수 있기 때문에
+				max[i][1] = max[i-1][0] + wine[i];
+				if(i+1<n)
+					max[i+1][0] = Math.max(max[i+1][0] ,Math.max(max[i-1][0], max[i-1][1]) + wine[i+1]);
+				if(i+2<n)
+					max[i+2][0] = Math.max(max[i+2][0], Math.max(max[i-1][0], max[i-1][1]) + wine[i+2]);
+
+			} 
+			System.out.println(Math.max(Math.max(max[n-2][0], max[n-2][1]), Math.max(max[n-1][0], max[n-1][1])));
+		}
+		
+	}
+}
