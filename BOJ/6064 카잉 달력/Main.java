e2b29ff4f75af26467cf3e5ab4e1139637c0d0d1
1hanhb <1hanhb@naver.com>
2017-12-25 22:10:58 +0900

    171225

--- BOJ/6064 카잉 달력/Main.java
+++ BOJ/6064 카잉 달력/Main.java
@@ -5,6 +5,11 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		int T = scanner.nextInt();
 		StringBuilder sb = new StringBuilder();
+		/* 카잉 달력 해결 ->
+		 * 1 1 -> 2 2 -> 3 3 -> 4 4 -> ... -> M*N번 반복
+		 * 여기서 각각 M과 N으로 %하면 현재 <x:y> 구하기 가능
+		 * 위의 규칙으로 ㄱㄱ
+		 */
 		Loop : for(int t=0; t<T; t++) {
 			int M = scanner.nextInt(),N = scanner.nextInt(),X = scanner.nextInt(),Y = scanner.nextInt();
 			for(int i=0; i*M+X<=M*N; i++) {
