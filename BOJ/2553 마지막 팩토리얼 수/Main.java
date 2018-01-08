1d4cae366b019e8c872df04bdcb5845d12bdf298
1hanhb <1hanhb@naver.com>
2018-01-08 18:08:58 +0900

    180108

---
+++ BOJ/2553 마지막 팩토리얼 수/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		long []nums = new long[N+1];
+		nums[0] = 1;
+		for(int i=1;i<=N;i++) {
+			nums[i] = nums[i-1]*i;
+			while(nums[i]%10==0) {
+				nums[i]/=10;
+			}
+			nums[i] = nums[i]%1000000000;
+		}
+		nums[N]=nums[N]%100000;
+		String result = "";
+		int count = 5-String.valueOf(nums[N]).length();
+		for(int i=0; i<count; i++)
+			result+="0";
+		System.out.println(result+nums[N]);
+	}
+}
