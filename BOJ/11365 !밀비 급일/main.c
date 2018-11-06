15002ab2b7bd0d03545f954cca12379d40d86523
hanhb1011 <1hanhb@naver.com>
2018-11-06 18:36:01 +0900

    181106

---
+++ BOJ/11365 !밀비 급일/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int i, len;
+char buf[555];
+
+main() {
+
+	fgets(buf, 555, stdin);
+
+	while (strcmp(buf, "END\n") != 0) {
+		len = strlen(buf);
+		for (i = len - 2; i >= 0; i--)
+			printf("%c", buf[i]);
+		printf("\n");
+		fgets(buf, 555, stdin);
+	}
+
+}
