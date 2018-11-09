221fb786188bc9b1e5d97e9f80b8a5e3db626f46
hanhb1011 <1hanhb@naver.com>
2018-11-09 15:55:19 +0900

    181109

---
+++ BOJ/3449 해밍 거리/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int t, i, len, c;
+char a[101], b[101];
+
+main() {
+	scanf("%d", &t);
+	while (t-- > 0) {
+		c = 0;
+		scanf("%s%s", a, b);
+		len = strlen(a);
+		for (i = 0; i < len; i++)
+			if (a[i] != b[i])
+				c++;
+		printf("Hamming distance is %d.\n", c);
+	}
+
+}
