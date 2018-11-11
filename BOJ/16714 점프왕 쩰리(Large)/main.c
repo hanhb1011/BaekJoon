3d8fc26ae102d2d7430cbd830e2ac746b5146046
hanhb1011 <1hanhb@naver.com>
2018-11-11 17:09:04 +0900

    181111

---
+++ BOJ/16714 점프왕 쩰리(Large)/main.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int N, a[99][99], v[99][99], i, j, Qy[100000], Qx[100000], fy, ry = 1, fx, rx = 1, dy[2] = { 1,0 }, dx[2] = { 0,1 }, y, x, n, ny, nx;
+
+main() {
+	scanf("%d", &N);
+	for (i = 0; i < N*N; i++)
+		scanf("%d", &a[i/N][i%N]);
+
+	while (fy < ry) {
+		y = Qy[fy++];
+		x = Qx[fx++];
+		n = a[y][x];
+		if (n == 0 || v[y][x] == 1)
+			continue;
+		v[y][x] = 1;
+
+		if (n == -1) {
+			printf("HaruHaru");
+			return 0;
+		}
+		
+		for (i = 0; i < 2; i++) {
+			ny = y + n * dy[i];
+			nx = x + n * dx[i];
+			if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+				Qy[ry++] = ny;
+				Qx[rx++] = nx;
+			}
+		}
+		
+	}
+	printf("Hing");
+}
---
+++ BOJ/3190 뱀/main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+int n, k, l, i, j, y, x, a[100][100], Qi[99999], Qj[99999], fi, ri = 1, fj, rj = 1, d, dir[100][2], t, next, dy[4] = { 0,1,0,-1 }, dx[4] = { 1,0,-1,0 }, r;
+char b;
+
+main() {
+	scanf("%d%d", &n, &k);
+	while (k-- > 0) {
+		scanf("%d%d", &y, &x);
+		a[y - 1][x - 1] = 1;
+	}
+	scanf("%d", &l);
+	for (i = 0; i < l; i++) {
+		scanf("%d%s", &j, &b);
+		dir[i][0] = j;
+		dir[i][1] = b == 'L' ? -1 : 1;
+	}
+	y = x = 0;
+	a[0][0] = 2;
+
+	while (1) {
+		t++;
+		y += dy[d];
+		x += dx[d];
+
+		if (y < 0 || y >= n || x < 0 || x >= n || a[y][x] == 2)
+			break;
+		else if (a[y][x] == 0)
+			a[Qi[fi++]][Qj[fj++]] = 0;
+
+		a[y][x] = 2;
+		Qi[ri++] = y;
+		Qj[rj++] = x;
+		if (next < l&&t == dir[next][0])
+			d = (d + dir[next++][1] + 4) % 4;
+	}
+	printf("%d", t);
+}
