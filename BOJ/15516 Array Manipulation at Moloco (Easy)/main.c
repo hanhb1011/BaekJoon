e4bcc9b21cc5363d7cdd172d9a2e9fce801be7a3
hanhb1011 <1hanhb@naver.com>
2018-11-14 16:58:11 +0900

    181114

---
+++ BOJ/15516 Array Manipulation at Moloco (Easy)/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int n, i,j,c,a[1000];
+main() {
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		c = 0;
+		scanf("%d", &a[i]);
+		for (j = 0; j < i; j++)
+			if (a[j] < a[i])
+				c++;
+		printf("%d\n", c);
+	}
+}
