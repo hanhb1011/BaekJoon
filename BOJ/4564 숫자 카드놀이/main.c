63d7c4e4f62444f51f6932509bef1c6995f19da4
hanhb1011 <1hanhb@naver.com>
2018-11-22 19:08:29 +0900

    181122

---
+++ BOJ/4564 숫자 카드놀이/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+main(n,a) {
+	scanf("%d", &n);
+
+	while (n != 0) {
+		while (n/10 > 0) {
+			printf("%d ", n);
+			a = n % 10;
+			while (n/10> 0) {
+				n /= 10;
+				a *= (n % 10);
+			}
+			n = a;
+		}
+
+		printf("%d\n",n);
+		scanf("%d", &n);
+	}
+
+}
