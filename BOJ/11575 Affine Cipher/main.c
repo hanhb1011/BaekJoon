8692fba9e8838a3dc52895ed4a41d3fd7ba50187
hanhb1011 <1hanhb@naver.com>
2018-12-16 16:18:14 +0900

    181216

---
+++ BOJ/11575 Affine Cipher/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int a, b, t, i,l;
+char s[1000001];
+
+main() {
+
+	scanf("%d", &t);
+	while (t-- > 0) {
+		scanf("%d%d%s", &a, &b, s);
+		l = strlen(s);
+
+		for (i = 0; i < l; i++)
+			s[i] = ((s[i] - 'A')*a + b) % 26 + 'A';
+		printf("%s\n", &s);
+	}
+}
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ BOJ problems
 
 > https://www.acmicpc.net/user/hanhb1011
 >
-> last update : 18.11.26.
+> last update : 18.12.16.
